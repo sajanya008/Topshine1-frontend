b7c71464ebd2b69058501f6301839e01a98cf048
sajanya008 <sajanyasudhakaran@gmail.com>
2026-03-31 13:07:26 +0530

    update

--- src/components/FOOTER/Footer.js
+++ src/components/FOOTER/Footer.js
@@ -41,6 +41,7 @@ const Footer = () => {
           <h4>Quick Links</h4>
           <ul>
             <li><Link to="/">Home</Link></li>
+            <li><Link to="/admin/login">Admin</Link></li>
             <li><Link to="/about">About Us</Link></li>
             <li><Link to="/service">Services</Link></li>
             <li><Link to="/pricepage">Pricing</Link></li>
